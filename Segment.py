cf400e3640b74adf2e961cbcb5d1691c5cd1f6de
Rahul <rahulkotr1@gmail.com>
2022-01-12 19:37:31 +0530

    Complete Segment

--- Segment.py
+++ Segment.py
@@ -28,3 +28,60 @@ def get_images_and_segments_test_arrays():
 
 # load the ground truth images and segmentation masks
 y_true_images, y_true_segments = get_images_and_segments_test_arrays()
+
+
+# get the model prediction
+results = model.predict(validation_dataset, steps=validation_steps)
+
+# for each pixel, get the slice number which has the highest probability
+results = np.argmax(results, axis=3)
+
+def compute_metrics(y_true, y_pred):
+  '''
+  Computes IOU and Dice Score.
+
+  Args:
+    y_true (tensor) - ground truth label map
+    y_pred (tensor) - predicted label map
+  '''
+  
+  class_wise_iou = []
+  class_wise_dice_score = []
+
+  smoothening_factor = 0.00001
+
+  for i in range(12):
+    intersection = np.sum((y_pred == i) * (y_true == i))
+    y_true_area = np.sum((y_true == i))
+    y_pred_area = np.sum((y_pred == i))
+    combined_area = y_true_area + y_pred_area
+    
+    iou = (intersection + smoothening_factor) / (combined_area - intersection + smoothening_factor)
+    class_wise_iou.append(iou)
+    
+    dice_score =  2 * ((intersection + smoothening_factor) / (combined_area + smoothening_factor))
+    class_wise_dice_score.append(dice_score)
+
+  return class_wise_iou, class_wise_dice_score
+
+# input a number from 0 to 63 to pick an image from the test set
+integer_slider = 0
+
+# compute metrics
+iou, dice_score = compute_metrics(y_true_segments[integer_slider], results[integer_slider])  
+
+# visualize the output and metrics
+show_predictions(y_true_images[integer_slider], [results[integer_slider], y_true_segments[integer_slider]], ["Image", "Predicted Mask", "True Mask"], iou, dice_score)
+
+# compute class-wise metrics
+cls_wise_iou, cls_wise_dice_score = compute_metrics(y_true_segments, results)
+
+# print IOU for each class
+for idx, iou in enumerate(cls_wise_iou):
+  spaces = ' ' * (13-len(class_names[idx]) + 2)
+  print("{}{}{} ".format(class_names[idx], spaces, iou)) 
+
+# print the dice score for each class
+for idx, dice_score in enumerate(cls_wise_dice_score):
+  spaces = ' ' * (13-len(class_names[idx]) + 2)
+  print("{}{}{} ".format(class_names[idx], spaces, dice_score)) 
