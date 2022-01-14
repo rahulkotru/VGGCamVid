860ec9c879168a1d8e66f10be3e2d8a0b654ddde
Rahul <rahulkotr1@gmail.com>
2022-01-14 11:21:38 +0530

    Cleaned Segment by removing unused variables

--- Segment.py
+++ Segment.py
@@ -3,12 +3,6 @@ from DataGen import *
 
 
 def get_images_and_segments_test_arrays():
-  '''
-  Gets a subsample of the val set as your test set
-
-  Returns:
-    Test set containing ground truth images and label maps
-  '''
   y_true_segments = []
   y_true_images = []
   test_count = 64
@@ -37,13 +31,7 @@ results = model.predict(validation_dataset, steps=validation_steps)
 results = np.argmax(results, axis=3)
 
 def compute_metrics(y_true, y_pred):
-  '''
-  Computes IOU and Dice Score.
 
-  Args:
-    y_true (tensor) - ground truth label map
-    y_pred (tensor) - predicted label map
-  '''
   
   class_wise_iou = []
   class_wise_dice_score = []
@@ -64,24 +52,18 @@ def compute_metrics(y_true, y_pred):
 
   return class_wise_iou, class_wise_dice_score
 
-# input a number from 0 to 63 to pick an image from the test set
 integer_slider = 0
 
-# compute metrics
 iou, dice_score = compute_metrics(y_true_segments[integer_slider], results[integer_slider])  
 
-# visualize the output and metrics
 show_predictions(y_true_images[integer_slider], [results[integer_slider], y_true_segments[integer_slider]], ["Image", "Predicted Mask", "True Mask"], iou, dice_score)
 
-# compute class-wise metrics
 cls_wise_iou, cls_wise_dice_score = compute_metrics(y_true_segments, results)
 
-# print IOU for each class
 for idx, iou in enumerate(cls_wise_iou):
   spaces = ' ' * (13-len(class_names[idx]) + 2)
   print("{}{}{} ".format(class_names[idx], spaces, iou)) 
 
-# print the dice score for each class
 for idx, dice_score in enumerate(cls_wise_dice_score):
   spaces = ' ' * (13-len(class_names[idx]) + 2)
   print("{}{}{} ".format(class_names[idx], spaces, dice_score)) 
